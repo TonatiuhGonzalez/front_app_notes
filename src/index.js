09c19661bf5638ad771760aa249a1b86ac0a38e0
TonatiuhGonzalez <gonz.tonatiuh@gmail.com>
2022-06-02 15:46:33 -0500

    Muestra los números de ejercicios

--- src/index.js
+++ src/index.js
@@ -15,8 +15,14 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2 ={part2} part3 ={part3}/>
+      <Header titulo={course}/>
+      <Content 
+        part1={part1}
+        exercises1={exercises1}
+        part2 ={part2}
+        exercises2={exercises2}
+        part3 ={part3}
+        exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3 ={exercises3}/>
     </div>
   )
@@ -25,7 +31,7 @@ const App = () => {
   const Header = (props) =>{
     return(
       <div>
-        <h1>{props.course}</h1>
+        <h1>{props.titulo}</h1>
       </div>
     )
   }
@@ -34,13 +40,13 @@ const App = () => {
     return(
       <div>
         <p>
-          {props.part1}
+          {props.part1} {props.exercises1}
         </p>
         <p>
-          {props.part2}
+          {props.part2} {props.exercises2}
         </p>
         <p>
-          {props.part3}
+          {props.part3} {props.exercises3}
         </p>
       </div>
     )
